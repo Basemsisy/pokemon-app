4107c9290ffba8e5d7ff398948f6146700d0e73f
Basem Elsisy <basemsisy@gmail.com>
2025-03-01 19:44:56 +0200

    Set up basic routing for Pokemon app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import PokemonList from "./components/PokemonList";
+import PokemonDetails from "./components/PokemonDetails";
 function App() {
-  return <div>hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PokemonList />} />
+        <Route path="/pokemon/:name" element={<PokemonDetails />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
